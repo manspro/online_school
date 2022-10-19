9beb973d4f0767bbafdfc5ca2112819ff13b815f
Mansur Makhmutov <sir.makhmutov@gmail.com>
2022-10-20 00:30:27 +0500

    create User interface

---
+++ school/libs/interfaces/src/lib/user.interface.ts
@@ -0,0 +1,13 @@
+export enum UserRole {
+  Teacher = 'Teacher',
+  Student = 'Student'
+}
+
+export interface IUser{
+  _id?: string;
+  displayName: string;
+  email: string;
+  passwordHash: string;
+  role: UserRole;
+
+}
