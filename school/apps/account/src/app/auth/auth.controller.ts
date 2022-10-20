018e1bb7544e9041b3adb27ec1e43cfc70f8e875
Mansur Makhmutov <sir.makhmutov@gmail.com>
2022-10-21 00:33:40 +0500

    add authService

--- school/apps/account/src/app/auth/auth.controller.ts
+++ school/apps/account/src/app/auth/auth.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthService} from "./auth.service";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService
+  ) {
+  }
+
+
+  @Post('register')
+  async register (@Body() dto: RegisterDto){
+  return this.authService.register(dto)
+  }
+
+  @Post('login')
+  async login (@Body() { email, password } : LoginDto) {
+    const { id } = await this.authService.validateUser(email, password)
+  }
+}
+
+export class RegisterDto{
+  displayName: string;
+  email: string;
+  password:string;
+}
+
+export class LoginDto{
+  email: string;
+  password:string;
+}
--- school/apps/account/src/app/auth/auth.service.ts
+++ school/apps/account/src/app/auth/auth.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {RegisterDto} from "./auth.controller";
+import {UserRepository} from "../user/repositories/user.repository";
+import {UserEntity} from "../user/entities/user.entity";
+import {UserRole} from "../../../../../libs/interfaces/src/lib/user.interface";
+import {User} from "../user/models/user.model";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userRepository: UserRepository
+  ) {
+  }
+
+  async register({email, password, displayName}: RegisterDto) {
+    const oldUser = this.userRepository.findUser(email)
+
+    if (oldUser) {
+      throw new Error('Такой пользователь уже существует')
+    }
+    const newUserEntity = await new UserEntity({
+      displayName,
+      email,
+      passwordHash: '',
+      role: UserRole.Student,
+    }).setPassword(password);
+    const newUser = await this.userRepository.createUser(newUserEntity)
+    return {email: newUser.email}
+  }
+
+    async validateUser(email:string, password:string){
+      const user = await this.userRepository.findUser(email)
+
+      if(!user){
+        throw new Error('Неверный логин или пароль')
+      }
+      const userEntity =  new UserEntity(user)
+      const correctPassword = await userEntity.validatePassword(password)
+
+      if(!correctPassword){
+        throw new Error('Неверный логин или пароль')
+      }
+      return {id: user._id}
+    }
+}
