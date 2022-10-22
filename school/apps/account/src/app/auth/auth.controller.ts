699fa4cca7558f605d620c801b96bbd6a9af6c31
Mansur Makhmutov <sir.makhmutov@gmail.com>
2022-10-22 22:29:29 +0500

    add rmq validate

--- school/apps/account/src/app/auth/auth.controller.ts
+++ school/apps/account/src/app/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import {Body, Controller} from '@nestjs/common';
 import {AuthService} from "./auth.service";
 import {AccountLogin} from "@school/contracts";
 import {AccountRegister} from "../../../../../libs/contracts/src/lib/account/account.register";
-import {RMQRoute} from "nestjs-rmq";
+import {RMQRoute, RMQValidate} from "nestjs-rmq";
 
 
 @Controller()
@@ -12,11 +12,13 @@ export class AuthController {
   ) {
   }
 
+  @RMQValidate()
   @RMQRoute(AccountRegister.topic)
   async register (@Body() dto: AccountRegister.Request) : Promise<AccountRegister.Response>{
   return this.authService.register(dto)
   }
-
+  
+  @RMQValidate()
   @RMQRoute(AccountLogin.topic)
   async login (@Body() { email, password } : AccountLogin.Request)  {
     const { id } = await this.authService.validateUser(email, password)
